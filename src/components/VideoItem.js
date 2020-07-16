f63469e9a1e846cf5edc3be7be9339d80ae1c19f
mennatallah.ramadan <mennatallah.ramadan@itworx.com>
2020-07-16 16:25:38 +0200

    working on videoItem and video list

---
+++ src/components/VideoItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+
+class VideoItem extends React.Component{
+    render(){
+        return(
+        <div>
+            VideoItem
+        </div>
+        )
+    }
+}
+
+export default VideoItem;
