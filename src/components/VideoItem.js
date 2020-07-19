368f3ebe1b3eb88c92b2a7d575b5c1250cbf4757
mennatallah.ramadan <mennatallah.ramadan@itworx.com>
2020-07-19 14:58:43 +0200

    working on react videos

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import './VideoItem.css';
 
-class VideoItem extends React.Component{
-    render(){
-        return(
-        <div>
-            VideoItem
+const VideoItem = ({video, onVideoSelect}) => {
+
+    return(
+        <div className="item video-item" onClick={() => onVideoSelect(video)}>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title}/>
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
-        )
-    }
+        ) 
 }
 
 export default VideoItem;
